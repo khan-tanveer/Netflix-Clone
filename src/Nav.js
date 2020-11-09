84d56d448ebb2470cae679cd473419ac86661fac
khan-tanveer <59610686+khan-tanveer@users.noreply.github.com>
2020-11-09 14:59:19 +0530

    netflix clone

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, []);
+
+
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+                className="nav__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
+            <img
+                className="nav__avatar"
+                src="https://as1.ftcdn.net/jpg/01/09/45/80/500_F_109458015_QsWmchlzuwCZPqIUWR7HcTDsbbptejRv.jpg"
+                alt="Netflix Logo" />
+
+        </div >
+    )
+}
+
+export default Nav;
